9d3490a0b2b3fc280404becc38540549f657f58e
Kavyansh Pancholi <kavyanshpancholi@gmail.com>
2024-12-29 19:10:50 +0530

    Db resolved+create game controller

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,9 +1,26 @@
-const createGame = (req, res) => {
-  res.json({ message: "create game route" });
+import Game from "../models/game.model.js";
+import shortid from "shortid";
+
+
+const createGame = async (req, res) => {
+    try {
+        const { initialAmount, password, maxPlayers } = req.body;
+        const name = shortid.generate();
+        const newGame = new Game({
+          initialAmount,
+          name,
+          password,
+          maxPlayers,
+        });
+        await newGame.save();
+        res.status(200).json(newGame);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 const getGame = (req, res) => {
-  res.json({ message: "get game route" });
+	res.json({ message: "get game route" });  
 };
 
 export { createGame, getGame };
