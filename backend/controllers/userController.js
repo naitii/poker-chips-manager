84e35b91ffb9edce8a2640b406318ebfd3a9dec3
Chirayu31 <chirayupancholi17@gmail.com>
2024-12-29 19:11:50 +0530

    create user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,18 @@
-const addUser = (req, res) => {
-  res.json({ message: "add user route" });
+import User from "../model/player.model.js";
+const addUser = async (req, res) => {
+  const userData = req.body;
+
+  if (!userData || !userData.name) {
+    res.status(400).json({ message: "Invalid data" });
+  }
+
+  try {
+    await User.create(userData);
+    res.json({ message: "User added successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error", error: error });
+  }
 };
 
 const getUser = (req, res) => {
