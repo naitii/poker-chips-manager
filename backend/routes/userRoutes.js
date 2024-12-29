efb05d1b9ad8b80998259b2f829b19cf2fa7ffa1
Chirayu31 <chirayupancholi17@gmail.com>
2024-12-29 19:34:22 +0530

    remove user routes

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as userController from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post("/", userController.addUser);
-router.get("/", userController.getUser);
-
-export default router;
