7c9bc9ca1f962227cfd018c4e4e2a164d82dd521
Chirayu31 <chirayupancholi17@gmail.com>
2024-12-29 19:14:06 +0530

    Revert "create user"
    This reverts commit 84e35b91ffb9edce8a2640b406318ebfd3a9dec3.

--- backend/model/player.model.js
+++ backend/model/player.model.js
@@ -1,9 +1,10 @@
-import mongoose from "mongoose";
-const { Schema } = mongoose;
+import mongoose from 'mongoose';
+const {Schema} = mongoose;
 
 const PlayerSchema = new Schema({
-  name: { type: String, required: true },
-  amount: { type: Number, required: true, default: 0 },
-});
+    id : {type: Number, required: true},
+    name : {type: String, required: true},
+    amount : {type: Number, required: true, default: 0},
+})
 
-export default mongoose.model("Player", PlayerSchema);
+export default mongoose.model('Player', PlayerSchema);
